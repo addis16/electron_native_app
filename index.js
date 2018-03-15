c1f2aecb855161b428a4eb1423594f780cef7e45
addis16 <j.addis@setonhill.edu>
2018-03-15 08:51:28 -0400

    App is opening on desktop properly

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ var BrowserWindow = electron.BrowserWindow;
 var app = electron.app;
 
 app.on('ready', function() {
-    var appWindow;
-    appWindow = new BrowserWindow();
+    var appWindow, infoWindow;
+    appWindow = new BrowserWindow({
+        show: false
+    });
     appWindow.loadURL('http://www.jacob-addis.com');
-})
+
+    infoWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        frame: false,
+        transparent: true,
+        show: false
+    });
+    infoWindow.loadURL('file://' + __dirname + '/info.html');
+
+    appWindow.once('ready-to-show', function() {
+        appWindow.show();
+        setTimeout(function() {
+            infoWindow.show();
+        }, 1000);
+    });
+});
