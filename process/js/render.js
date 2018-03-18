f0b70395873ff0148689717375804c30108d9b69
addis16 <j.addis@setonhill.edu>
2018-03-18 17:29:19 -0400

    Created react component

--- process/js/render.js
+++ process/js/render.js
@@ -3,13 +3,46 @@ var ReactDOM = require('react-dom');
 
 var $ = jQuery = require('jquery');
 var bootstrap = require('bootstrap');
+var fs = eRequire('fs');
+var loadApts = JSON.parse(fs.readFileSync(dataLocation));
 
 var MainInterface = React.createClass({
-   render: function() {
-       return(
-           <h1>React Application</h1>
-       );
-   } //render
+  getInitialState: function() {
+    return {
+      myAppointments: loadApts
+    }
+  },
+  render: function() {
+    var myAppointments = this.state.myAppointments;
+     return(
+
+       <div className="application">
+        <div className="container">
+         <div className="row">
+           <div className="appointments col-sm-12">
+             <h2 className="appointments-headline">Current Appointments</h2>
+             <ul className="item-list media-list">
+
+               <li className="pet-item media">
+                 <div className="pet-info media-body">
+                   <div className="pet-head">
+                     <span className="pet-name">{myAppointments[0].petName}</span>
+                     <span className="apt-date pull-right">{myAppointments[0].aptDate}</span>
+                   </div>
+                   <div className="owner-name"><span className="label-item">Owner:</span>
+                   {myAppointments[0].ownerName}</div>
+                   <div className="apt-notes">{aptNotes}</div>
+                 </div>
+               </li>
+
+             </ul>
+           </div>{/* col-sm-12 */}
+         </div>{/* row */}
+        </div>{/* container */}
+      </div>
+
+     );
+  } //render
 }); //MainInterface
 
 ReactDOM.render(
