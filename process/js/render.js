2dc6e787cb74a178283b9b3361f0ad86a11dd4f1
addis16 <j.addis@setonhill.edu>
2018-03-18 21:28:12 -0400

    handled deleting data from DOM

--- process/js/render.js
+++ process/js/render.js
@@ -1,4 +1,5 @@
 var $ = jQuery = require('jquery');
+var _ = require('lodash');
 var bootstrap = require('bootstrap');
 var fs = eRequire('fs');
 var loadApts = JSON.parse(fs.readFileSync(dataLocation));
@@ -13,33 +14,52 @@ var MainInterface = React.createClass({
       myAppointments: loadApts
     }
   },
+
+  deleteMessage: function(item) {
+    var allApts = this.state.myAppointments;
+    var newApts = _.without(allApts, item);
+    this.setState({
+      myAppointments: newApts
+    });
+  },
+
   render: function() {
     var myAppointments = this.state.myAppointments;
 
     myAppointments = myAppointments.map(function(item, index) {
       return(
+
         <AptList key = {index}
           singleItem = {item}
+          whichItem = {item}
+          onDelete = {this.deleteMessage}
         />
+
       )
-    }.bind(this)); // Appointments.map
+    }.bind(this));
 
     return (
+
       <div className="application">
         <div className="container">
           <div className="row">
             <div className="appointments col-sm-12">
               <h2 className="appointments-headline">Current Appointments</h2>
-              <ul className="item-list media-list">{myAppointments}</ul>
-            </div>{/* col-sm-12 */}
-          </div>{/* row */}
-        </div>{/* container */}
+              <ul className="item-list media-list">
+
+                {myAppointments} 
+
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
+
     );
-  } //render
-}); //MainInterface
+  }
+});
 
 ReactDOM.render(
     <MainInterface />,
     document.getElementById('petAppointments')
-); //render
+);
